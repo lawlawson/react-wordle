b4565256453665730929acf1ca04f1827e86910f
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-04-23 00:03:41 +0100

    Added empty functions within useWordle

---
+++ src/hooks/useWordle.js
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+const useWordle = (solution) => {
+  const formatGuess = () => {};
+  const addNewGuess = () => {};
+  const handleKeyup = () => {};
+};
+
+export default useWordle;
