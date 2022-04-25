f221e76e7ba50a55d78e2015e4a22e09af0965a6
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-04-25 16:38:14 +0100

    Checking guesses

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -4,41 +4,60 @@ const useWordle = (solution) => {
   const [turn, setTurn] = useState(0);
   const [currentGuess, setCurrentGuess] = useState('');
   const [guesses, setGuesses] = useState([]);
-  const [history, setHistory] = useState(['ninja']);
+  const [history, setHistory] = useState([]);
   const [isCorrect, setIsCorrect] = useState(false);
 
   const formatGuess = () => {
-    console.log('Formating the guess - ', currentGuess);
+    let solutionArray = [...solution];
+    let formattedGuess = [...currentGuess].map((l) => {
+      return { key: l, color: 'grey' };
+    });
+
+    formattedGuess.forEach((l, i) => {
+      if (solution[i] === l.key) {
+        formattedGuess[i].color = 'green';
+        solutionArray[i] = null;
+      }
+    });
+
+    formattedGuess.forEach((l, i) => {
+      if (solutionArray.includes(l.key) && l.color !== 'green') {
+        formattedGuess[i].color = 'yellow';
+        solutionArray[solutionArray.indexOf(l.key)] = null;
+      }
+    });
+
+    return formattedGuess;
   };
+
   const addNewGuess = () => {};
+
   const handleKeyup = ({ key }) => {
     if (key === 'Enter') {
       if (turn > 5) {
-        console.log('You used all your guesses');
-        return;
-      }
-      if (history.includes(currentGuess)) {
-        console.log('You already tried that word');
-        return;
-      }
-      if (currentGuess.length !== 5) {
-        console.log('Word must be at least 5 characters');
+        console.log('you used all your guesses!');
         return;
       }
 
-      formatGuess();
+      if (history.includes(currentGuess)) {
+        console.log('you already tried that word.');
+        return;
+      }
+
+      if (currentGuess.length !== 5) {
+        console.log('word must be 5 chars.');
+        return;
+      }
+      const formatted = formatGuess();
+      console.log(formatted);
     }
     if (key === 'Backspace') {
-      setCurrentGuess((prev) => {
-        return prev.slice(0, -1);
-      });
+      setCurrentGuess((prev) => prev.slice(0, -1));
+      return;
     }
-
     if (/^[A-Za-z]$/.test(key)) {
       if (currentGuess.length < 5) {
-        setCurrentGuess((prev) => {
-          return prev + key;
-        });
+        setCurrentGuess((prev) => prev + key);
       }
     }
   };
