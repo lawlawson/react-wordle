ed11c4797f46792ae3af11dda2e09c4b49eb3d09
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-04-28 23:06:32 +0100

    Added Keypad component

---
+++ src/components/Keypad.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+export default function Keypad() {
+  const [letters, setLetters] = useState(null);
+
+  useEffect(() => {});
+
+  return <div>Keypad</div>;
+}
