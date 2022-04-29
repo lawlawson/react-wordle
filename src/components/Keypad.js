849b8f48c040946c2b03db60ff5cf76407c9ea21
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-04-29 14:32:20 +0100

    Added keypad and styling

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -3,7 +3,20 @@ import React, { useState, useEffect } from 'react';
 export default function Keypad() {
   const [letters, setLetters] = useState(null);
 
-  useEffect(() => {});
+  useEffect(() => {
+    fetch('http://localhost:3001/letters')
+      .then((res) => res.json())
+      .then((json) => {
+        setLetters(json);
+      });
+  }, []);
 
-  return <div>Keypad</div>;
+  return (
+    <div className='keypad'>
+      {letters &&
+        letters.map((l) => {
+          return <div key={l.key}>{l.key}</div>;
+        })}
+    </div>
+  );
 }
--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,6 +1,7 @@
 import { React, useEffect } from 'react';
 import useWordle from '../hooks/useWordle';
 import Grid from './Grid';
+import Keypad from './Keypad';
 
 export default function Wordle({ solution }) {
   const { currentGuess, handleKeyup, guesses, isCorrect, turn } =
@@ -21,6 +22,7 @@ export default function Wordle({ solution }) {
       <div>Solution - {solution}</div>
       <div>Current Guess - {currentGuess}</div>
       <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
+      <Keypad />
     </>
   );
 }
